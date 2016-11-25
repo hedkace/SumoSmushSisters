7c0831c6181ace6b9e9c409fd8f24582d509def5
aka: cauzality, zeno evil <cauzality@comcast.net>
2016-11-25 17:17:16 -0600

    upload
    first

---
+++ Assets/CamCon.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamCon : MonoBehaviour {
+
+    public GameObject player1;
+    public GameObject player2;
+    private Vector3 focus;
+
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        focus = (player1.transform.position + player2.transform.position) / 2f;
+        transform.position = focus + Vector3.back*setD(player1.transform.position,player2.transform.position) + Vector3.up*5f;
+        transform.LookAt(focus);
+
+    }
+
+
+
+    float setD(Vector3 p1position, Vector3 p2position)
+    {
+        float distance = (p1position - p2position).magnitude;
+
+        return 20f + 20f / (1 + Mathf.Exp(20f - distance)) + 20f / (1 + Mathf.Exp(60f - distance));
+    }
+}
---
+++ Assets/PlayerCon.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCon : MonoBehaviour {
+
+    public float dToGround;
+    public float dJump;
+    public float gValue;
+    public float friction;
+    public float jump;
+    public float speed;
+    public float dFix;
+
+
+    private bool bGoingSomewhere;
+    private Quaternion direction;
+    private Quaternion quat;
+    private Rigidbody rb;
+    private Vector3 gEffect;
+    private float fEffect;
+    private bool bDoubleJump;
+
+	// Use this for initialization
+	void Start () {
+        direction = Quaternion.LookRotation(Vector3.right, Vector3.up);
+	    rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        //*** Start Spin Action
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, direction, 10f);
+
+        if (Input.GetButton("Horizontal")) {
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                direction = Quaternion.LookRotation(Vector3.right, Vector3.up);
+            }
+            else
+            {
+                direction = Quaternion.LookRotation(Vector3.left, Vector3.up);
+            }
+        }
+        //*** End Spin Action
+
+
+        //*** Start Controls
+        rb.velocity *= .99f;
+        if (bDoubleJump)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                rb.AddForce(Vector3.up * 1200f * jump);
+                bDoubleJump = false;
+            }
+        }
+        bGoingSomewhere = Input.GetButton("Horizontal");
+        if (bGoingSomewhere)
+        {
+            rb.velocity += transform.forward * .1f;
+        }
+        gEffect = Vector3.down / 2f * gValue;
+        RaycastHit hit;
+        Debug.Log(Physics.Raycast(new Ray(transform.position, Vector3.down), out hit));
+        try
+        {
+            if (Physics.Raycast(new Ray(transform.position, Vector3.down), out hit))
+            {
+                Debug.Log(hit.distance);
+                if (hit.distance < dJump)
+                {
+                    if (hit.collider.tag.Contains("Ground") /*&& rb.velocity.y < 0*/)
+                    {
+                        bDoubleJump = false;
+                        //rb.velocity = Vector3.ProjectOnPlane(rb.velocity, Vector3.up);
+                        gEffect = Vector3.zero;
+                        rb.velocity *= .9f;
+                        //rb.MovePosition(transform.position - Vector3.up * (dFix - hit.distance)); 
+                        //Jumping
+                        if (Input.GetButtonDown("Jump"))
+                        {
+                            rb.AddForce(Vector3.up * 1000f * jump);
+                            bDoubleJump = true;
+                        }
+                        //Movement
+                        if (bGoingSomewhere)
+                        {
+                            rb.velocity += transform.forward * 1.75f * speed;
+                        }
+                    }
+                }
+            }
+        }catch
+        {
+
+        }
+        Debug.Log(gEffect);
+        rb.velocity += gEffect;
+
+        //*** End Controls
+
+
+
+
+        //*** Start Boundary Check
+
+        //Check if fall off
+        if (transform.position.y < -30f)
+        {
+            transform.position = Vector3.up * 10f;
+            rb.velocity = Vector3.zero;
+        }
+        //Check if off sides
+
+        //*** End Boundary Check
+
+
+        //*** Start Region
+        //*** End Region
+
+
+        //*** Start Region
+        //*** End Region
+
+
+
+
+
+
+
+
+    }
+
+}
